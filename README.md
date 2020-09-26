dc0bb3a764a281df3b4078ca4046c56d3973694b
linxueyu <lin2006yuo@gmail.com>
2020-09-27 01:59:09 +0800

    bug

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 [preview](https://graph-8qv16umy2.vercel.app/)  
 发育中...   
+
+---
+### mark
+
+GraphCanvas(canvas, graph)
+- graph.attachCanvas将graph和graphcanvas关联
+- setCanvas导入canvas对象
+- bindEvent
+- startRending
+- draw *requestAnimation持续绘制
+- ds.computeVisibleArea 计算可视区域
+- drawBackCanvas *网格
+- drawFrontCanvase
--- graph.js
+++ graph.js
@@ -1,24 +1,41 @@
 class Graph {
   constructor() {}
   attachCanvas(graphcanvas) {
-    if(graphcanvas.constructor !== GraphCanvas) {
-      throw 'attachCanvas expects a GraphCanvas instance'
+    if (graphcanvas.constructor !== GraphCanvas) {
+      throw "attachCanvas expects a GraphCanvas instance"
     }
-    if(graphcanvas.graph && graphcanvas.graph !== this) {
+    if (graphcanvas.graph && graphcanvas.graph !== this) {
       graphcanvas.graph.detachCanvas(graphcanvas)
     }
     graphcanvas.graph = this
-    if(!this.list_of_graphcanvas) {
+    if (!this.list_of_graphcanvas) {
       this.list_of_graphcanvas = []
     }
     this.list_of_graphcanvas.push(graphcanvas)
   }
   detachCanvas(graphcanvas) {}
+  change() {
+    this.sendActionToCanvas("setDirty", [true, true])
+  }
+  sendActionToCanvas(action, params) {
+    if (!this.list_of_graphcanvas) return
+    for (var i = 0; i < this.list_of_graphcanvas.length; ++i) {
+      let c = this.list_of_graphcanvas[i]
+      if (c[action]) {
+        c[action].apply(c, params)
+      }
+    }
+  }
 }
 class DragAndScale {
   constructor() {
     this.visible_area = new Float32Array(4)
     this.element = null
+    this.scale = 1
+    this.last_mouse = [0, 0]
+    this.min_scale = 0.1
+    this.max_scale = 10
+    this.offset = new Float32Array([0, 0])
   }
   computeVisibleArea() {
     if (!this.element) {
@@ -27,23 +44,54 @@ class DragAndScale {
     }
     var width = this.element.width
     var height = this.element.height
-    // var startx = -this.offset[0]
-    // var starty = -this.offset[1]
-    // var endx = startx + width / this.scale
-    // var endy = starty + height / this.scale
-    var startx = -0
-    var starty = -0
-    var endx = width
-    var endy = height
+    var startx = -this.offset[0]
+    var starty = -this.offset[1]
+    var endx = startx + width / this.scale
+    var endy = starty + height / this.scale
+    // var startx = -0
+    // var starty = -0
+    // var endx = width
+    // var endy = height
     this.visible_area[0] = startx
     this.visible_area[1] = starty
     this.visible_area[2] = endx - startx
     this.visible_area[3] = endy - starty
   }
+  changeScale(value, zooming_center) {
+    if (value < this.min_scale) {
+      value = this.min_scale
+    } else if (value > this.max_scale) {
+      value = this.max_scale
+    }
+    if (value === this.scale) {
+      return
+    }
+    if (!this.element) return
+    let rect = this.element.getBoundingClientRect()
+    if (!rect) return
+    zooming_center = zooming_center || [rect.width * 0.5, rect.height * 0.5]
+    let center = this.converCanvasToOffset(zooming_center)
+    this.scale = value
+    let new_center = this.converCanvasToOffset(zooming_center)
+    this.scale = value
+    let delta_offset = [new_center[0] - center[0], new_center[1] - center[1]]
+    this.offset[0] += delta_offset[0]
+    this.offset[1] += delta_offset[1]
+  }
+  converCanvasToOffset(pos, out) {
+    out = out || [0, 0]
+    out[0] = pos[0] / this.scale - this.offset[0]
+    out[1] = pos[1] / this.scale - this.offset[1]
+    return out
+  }
+  toCanvasContext(ctx) {
+    ctx.scale(this.scale, this.scale)
+    ctx.translate(this.offset[0], this.offset[1])
+  }
 }
 class GraphCanvas {
-  static DEFAULT_BACKGROUND_IMAGE =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAQBJREFUeNrs1rEKwjAUhlETUkj3vP9rdmr1Ysammk2w5wdxuLgcMHyptfawuZX4pJSWZTnfnu/lnIe/jNNxHHGNn//HNbbv+4dr6V+11uF527arU7+u63qfa/bnmh8sWLBgwYJlqRf8MEptXPBXJXa37BSl3ixYsGDBMliwFLyCV/DeLIMFCxYsWLBMwSt4Be/NggXLYMGCBUvBK3iNruC9WbBgwYJlsGApeAWv4L1ZBgsWLFiwYJmCV/AK3psFC5bBggULloJX8BpdwXuzYMGCBctgwVLwCl7Be7MMFixYsGDBsu8FH1FaSmExVfAxBa/gvVmwYMGCZbBg/W4vAQYA5tRF9QYlv/QAAAAASUVORK5CYII="
+  static DEFAULT_BACKGROUND_IMAGE = ""
+  // "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAQBJREFUeNrs1rEKwjAUhlETUkj3vP9rdmr1Ysammk2w5wdxuLgcMHyptfawuZX4pJSWZTnfnu/lnIe/jNNxHHGNn//HNbbv+4dr6V+11uF527arU7+u63qfa/bnmh8sWLBgwYJlqRf8MEptXPBXJXa37BSl3ixYsGDBMliwFLyCV/DeLIMFCxYsWLBMwSt4Be/NggXLYMGCBUvBK3iNruC9WbBgwYJlsGApeAWv4L1ZBgsWLFiwYJmCV/AK3psFC5bBggULloJX8BpdwXuzYMGCBctgwVLwCl7Be7MMFixYsGDBsu8FH1FaSmExVfAxBa/gvVmwYMGCZbBg/W4vAQYA5tRF9QYlv/QAAAAASUVORK5CYII="
   constructor(canvas, graph) {
     this.bgctx = null
     this.bgcanvas = null
@@ -52,9 +100,12 @@ class GraphCanvas {
     this.canvas = null
     this._mousemove_callback = null
     this._mouseup_callback = null
+    this.dirty_bgcanvas = true
+    this.clear_background = true
     this.background_image = GraphCanvas.DEFAULT_BACKGROUND_IMAGE
     this.visible_area = this.ds.visible_area
-    if(graph) {
+    this.last_mouse_position = [0, 0]
+    if (graph) {
       graph.attachCanvas(this)
     }
     this.setCanvas(canvas)
@@ -92,69 +143,178 @@ class GraphCanvas {
       }
     }
   }
-  draw() {
+  draw(force_canvas, force_bgcanvas) {
     if (!this.canvas || this.canvas.width == 0 || this.canvas.height == 0) {
       return
     }
     if (this.graph) {
       this.ds.computeVisibleArea()
     }
-    this.drawBackCanvas()
-    this.drawFrontCanvas()
+    if (this.dirty_bgcanvas || force_bgcanvas) {
+      this.drawBackCanvas()
+    }
+    if (this.dirty_canvas || force_canvas) {
+      this.drawFrontCanvas()
+    }
   }
   drawFrontCanvas() {
+    this.dirty_canvas = false
+
     if (!this.ctx) {
       this.ctx = this.bgcanvas.getContext("2d")
     }
-    let ctx = this.ctx
-    let canvas = this.canvas
+    var ctx = this.ctx
+    if (!ctx) {
+      //maybe is using webgl...
+      return
+    }
+
+    var canvas = this.canvas
+
+    //clear
+    //canvas.width = canvas.width;
+    if (this.clear_background) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+    }
+
+    //draw bg canvas
     if (this.bgcanvas == this.canvas) {
       this.drawBackCanvas()
     } else {
       ctx.drawImage(this.bgcanvas, 0, 0)
     }
+
+    //info widget
+    // ctx.translate(1, 1);
+
+    if (this.graph) {
+      //apply transformations
+      this.ds.toCanvasContext(ctx)
+    }
   }
   drawBackCanvas() {
+    let canvas = this.bgcanvas
+    if (
+      canvas.width != this.canvas.width ||
+      canvas.height != this.canvas.height
+    ) {
+      canvas.width = this.canvas.width
+      canvas.height = this.canvas.height
+    }
+
     if (!this.bgctx) {
       this.bgctx = this.bgcanvas.getContext("2d")
     }
     let ctx = this.bgctx
     let bg_already_painted = false
-    if (this.background_image && !bg_already_painted) {
-      ctx.imageSmoothingEnabled = ctx.mozImageSmoothingEnabled = ctx.imageSmoothingEnabled = false
-      if (!this._bg_img) {
-        this._bg_img = new Image()
-        this._bg_img.src = this.background_image
-        // this._bg_img.onload = function () {
-        //   console.log("loaded")
-        // }
-      }
-      let pattern = null
-      if (this._pattern == null && this._bg_img.width > 0) {
-        pattern = ctx.createPattern(this._bg_img, "repeat")
-        this._pattern_img = this._bg_img
-        this._pattern = pattern
-      } else {
-        this._pattern = pattern
-      }
 
-      if (pattern) {
-        ctx.fillStyle = pattern
-        ctx.fillRect(
-          this.visible_area[0],
-          this.visible_area[1],
-          this.visible_area[2],
-          this.visible_area[3]
-        )
-        ctx.fillStyle = "transparent"
-      }
-      ctx.globalAlpha = 1.0
-      ctx.imageSmoothingEnabled = true
+    if (this.clear_background) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
     }
+
+    //
+    // ctx.setTransform(1, 0, 0, 1, 0, 0)
+
+    if (this.graph) {
+      // this.ds.toCanvasContext(ctx)
+
+      if (this.background_image && !bg_already_painted) {
+        ctx.imageSmoothingEnabled = ctx.mozImageSmoothingEnabled = ctx.imageSmoothingEnabled = false
+        if (!this._bg_img) {
+          this._bg_img = new Image()
+          this._bg_img.src = this.background_image
+          let that = this
+          this._bg_img.onload = function () {
+            that.draw(true, true)
+          }
+        }
+
+        let pattern = null
+
+        if (this._pattern == null && this._bg_img.width > 0) {
+          pattern = ctx.createPattern(this._bg_img, "repeat")
+          this._pattern_img = this._bg_img
+          this._pattern = pattern
+        } else {
+          pattern = this._pattern
+        }
+
+        if (pattern) {
+          console.log(
+            "pattern",
+            this.ds.scale,
+            this.bgcanvas.width,
+            this.bgcanvas.height
+          )
+          ctx.fillStyle = pattern
+          ctx.fillRect(
+            // this.visible_area[0],
+            // this.visible_area[1],
+            // this.visible_area[2],
+            // this.visible_area[3]
+            0,
+            0,
+            500,
+            500
+          )
+          ctx.fillStyle = "transparent"
+        }
+        ctx.globalAlpha = 1.0
+        ctx.imageSmoothingEnabled = ctx.mozImageSmoothingEnabled = ctx.imageSmoothingEnabled = true
+      }
+    }
+    this.dirty_bgcanvas = true
+    this.dirty_canvas = true
   }
+  bindEvents() {
+    if (this._event_binded) {
+      console.log("GraphCanvas: events already binded")
+      return
+    }
+    let canvas = this.canvas
+    let window = this.getCanvasWindow()
+    this._mousewheel_callback = this.processMouseWheel.bind(this)
+    canvas.addEventListener("mousewheel", this._mousewheel_callback)
+    this._event_binded = true
+  }
+
   processMouseMove() {}
+
   processMouseUp() {}
-  bindEvents() {}
+
+  processMouseWheel(e) {
+    if (!this.graph) return
+    let delta = e.wheelDeltaY
+    this.adjustMouseEvent(e)
+    let scale = this.ds.scale
+    if (delta > 0) {
+      scale *= 1.1
+    } else if (delta < 0) {
+      scale *= 1 / 1.1
+    }
+    this.ds.changeScale(scale, [e.localX, e.localY])
+    this.graph.change()
+  }
+
+  adjustMouseEvent(e) {
+    if (this.canvas) {
+      var b = this.canvas.getBoundingClientRect()
+      e.localX = e.clientX - b.left
+      e.localY = e.clientY - b.top
+    } else {
+      e.localX = e.clientX
+      e.localY = e.clientY
+    }
+
+    // wheel deltaX只读属性
+    if (e.type !== "wheel") {
+      this.last_mouse_position[0] = e.localX
+      this.last_mouse_position[1] = e.localY
+    }
+
+    e.canvasX = e.localX / this.ds.scale - this.ds.offset[0]
+    e.canvasY = e.localY / this.ds.scale - this.ds.offset[1]
+  }
   /** 根据父元素的宽高 */
   resize(width, height) {
     if (!width & !height) {
@@ -169,8 +329,19 @@ class GraphCanvas {
     this.canvas.height = height
     this.bgcanvas.width = this.canvas.width
     this.bgcanvas.height = this.canvas.height
+    this.setDirty(true, true)
   }
+
   getCanvasWindow() {
     return window
+  }
+
+  setDirty(fgcanvas, bgcanvas) {
+    if (fgcanvas) {
+      this.dirty_canvas = true
+    }
+    if (bgcanvas) {
+      this.dirty_bgcanvas = true
+    }
   }
 }
