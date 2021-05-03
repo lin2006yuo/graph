37e3a3e4d7e21402477c59076740cf984242cd2f
linxueyu <lin2006yuo@gmail.com>
2021-05-03 13:17:50 +0800

    feat: 2

--- test/size.js
+++ test/size.js
@@ -1,13 +1,12 @@
 function KSize(canvas_id, options) {
   let canvas = document.getElementById(canvas_id)
   this.canvas = canvas
-  this.height = 0
-  this.width = 0
+  this.ratio = 2
   this.options = {
     onResize: () => {},
     ...options
   }
-  canvas.style.border = "1px solid #000"
+  canvas.style.border = `1px solid #000`
   if (canvas) {
     this.resize()
   }
@@ -16,8 +15,11 @@ function KSize(canvas_id, options) {
 KSize.prototype.resize = function () {
   let height = document.documentElement.clientHeight
   let width = document.documentElement.clientWidth
-  this.canvas.height = height / 2
-  this.canvas.width = width / 2
+  this.canvas.height = this.options.width * this.ratio || height
+  this.canvas.width = this.options.height * this.ratio || width
+  this.canvas.style.width = `${this.canvas.width / this.ratio}px`
+  this.canvas.style.height = `${this.canvas.height / this.ratio}px`
+
   this.height = this.canvas.height
   this.width = this.canvas.width
 
